824df7d143d55cc7a10f1d9e26f5d40155ff08bd
aadarshjha <jha.aadarsh@gmail.com>
2019-12-20 22:05:00 -0600

    Updates to Todo list

--- src/reminders.js
+++ src/reminders.js
@@ -1,5 +1,27 @@
 //script for the reminders.
 const memory = new Memory();
+const prompt = require('electron-prompt');
+
+function edit() {
+    prompt({
+      title: 'Enter Reminder',
+      label: 'Enter a reminder:',
+      value: 'Enter reminder here.',
+      type: 'input'
+  })
+  .then((r) => {
+      if(r === null) {
+          console.log('user cancelled');
+      } else {
+          fillRem(r);
+      }
+  })
+  .catch(console.error);
+}
+
+function fillRem(reminder){
+  
+}
 
 function createReminder() {
     const region = document.getElementById('drag');
@@ -33,9 +55,22 @@ button.addEventListener("click", createReminder);
 function remove(context){
   //this holds context; go two up two delete context.
   const removeRem = context.parentNode.parentNode;
-  console.log(removeRem);
-  removeRem.classList.add('hide');
+  //removes from stored reminder count:
+  memory.store('number', memory.get('number') - 1);
+  console.log(memory.get('number'));
+  removeRem.style.cssText = "opacity: 0; height: 0em;";
+
+  setTimeout(function(){
+    //gets index of the div content.
+    let childIndex = Array.prototype.indexOf.call(removeRem.parentNode.children, removeRem);
+
+    //clean up DOM;
+    removeRem.parentNode.removeChild(removeRem.parentNode.children[childIndex]);
+  }, 500);
+
 }
+
+
 
 //max of eight reminders for now for:
 function limitRem(){
